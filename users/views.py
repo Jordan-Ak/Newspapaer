483a2d73ef9e86ca33db75e71894a57e39806bf6
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-02-18 16:56:20 +0100

    Made custom templates, urls, views.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/signup.html'
