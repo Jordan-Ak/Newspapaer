25f9e7bf7a62465de27b1e6a76780b9e81b15db3
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-02-18 11:35:54 +0100

    editted custom admin user.

--- users/admin.py
+++ users/admin.py
@@ -10,5 +10,12 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    
+    list_display = ['email', 'username', 'age', 'is_staff', ]
+    fieldsets = UserAdmin.fieldsets + ( # new
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + ( # new
+        (None, {'fields': ('age',)}),
+    )
+
 admin.site.register(CustomUser, CustomUserAdmin)
