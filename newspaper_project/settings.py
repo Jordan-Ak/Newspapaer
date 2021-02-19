a5cb97e19a271a61a6fa93d67331bdf2fd828852
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-02-19 23:20:10 +0100

    Saved changes.

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER =  'testuseradmin@email.com'#This host user is your gmail  email address
-EMAIL_HOST_PASSWORD ='*******'#This password is your gmail password
+EMAIL_HOST_PASSWORD ='*******'#This password is your gmail password 
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
